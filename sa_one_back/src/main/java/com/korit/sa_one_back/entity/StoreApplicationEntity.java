bc01b9c8081846b58abfb6a262100224e29f94d4
wan03224 <dongwan03224@gmail.com>
2026-01-16 12:21:55 +0900

    feat(store): store, store application entity 추가

---
+++ sa_one_back/src/main/java/com/korit/sa_one_back/entity/StoreApplicationEntity.java
@@ -0,0 +1,24 @@
+package com.korit.sa_one_back.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class StoreApplicationEntity {
+    private Long storeApplicationId;
+    private String storeName;
+    private String businessNumber;
+    private String ownerName;
+    private String storePhone;
+    private String rejectReason;
+    private LocalDateTime reviewedAt;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+}
