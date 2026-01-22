6c7ebee3458acdf05808ad124c26cd7287f97536
wan03224 <dongwan03224@gmail.com>
2026-01-22 22:30:36 +0900

    fix(payroll): 필요없는 파일 삭제

--- sa_one_back/src/main/java/com/korit/sa_one_back/dto/request/payroll/StoreBusinessInfoDto.java
+++
@@ -1,9 +0,0 @@
-package com.korit.sa_one_back.dto.request.payroll;
-
-import lombok.Data;
-
-@Data
-public class StoreBusinessInfoDto {
-    private String companyType;
-    private String industryCode;
-}
--- sa_one_back/src/main/java/com/korit/sa_one_back/dto/request/payroll/TaxRateDto.java
+++
@@ -1,9 +0,0 @@
-package com.korit.sa_one_back.dto.request.payroll;
-
-import lombok.Data;
-
-@Data
-public class TaxRateDto {
-    private double incomeTaxRate;
-    private double localTaxRate;
-}
