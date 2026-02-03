1996e0d30076428368c5e85b3c314752e0ecd73e
wan03224 <dongwan03224@gmail.com>
2026-02-03 10:28:58 +0900

    Delete sa_one_back/src/main/java/com/korit/sa_one_back/mapper/admin/AdminPolicyMapper.java

--- sa_one_back/src/main/java/com/korit/sa_one_back/mapper/admin/AdminPolicyMapper.java
+++
@@ -1,8 +0,0 @@
-package com.korit.sa_one_back.mapper.admin;
-
-import com.korit.sa_one_back.entity.payroll.PayPolicyEntity;
-import org.apache.ibatis.annotations.Mapper;
-
-@Mapper
-public interface AdminPolicyMapper {
-}
